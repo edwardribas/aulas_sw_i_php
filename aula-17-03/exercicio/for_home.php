054755430885f63892a0535fffa45ed83d29eb56
Eduardo Ribas <ribasedu2005@gmail.com>
2022-03-17 09:07:43 -0300

    Operadores lógicos, switch e condicionais.

---
+++ aula-17-03/exercicio/for_home.php
@@ -0,0 +1,16 @@
+<?php
+
+    /*
+    
+    Elabore um programa que pergunte ao usuário em que turno ele estuda.
+    Peça para digitar 'M' para manhã, 'T' para tarde ou 'N' para noite.
+    Imprima a mensagem "Bom dia!", caso ele digite M;
+    "Boa tarde!", caso tenha digitado T;
+    "Boa noite!" se digitou N;
+
+    Caso ele tenha escrito um dado inválido, mostre um erro;
+    Elabore um formulário para receber a opção do usuário e uma página para mostrar a informação.
+
+    */
+
+?>
---
+++ aula-17-03/exercicio/process.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <!-- Metas -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- General -->
+    <link rel="stylesheet" href="style.css">
+    <title>Processamento</title>
+</head>
+<body>
+
+    <?php
+    
+        // soon
+
+    ?>
+
+</body>
+</html>
