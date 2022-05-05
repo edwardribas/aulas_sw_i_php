65826123d5f7815ca189538f39295a17faccd2c3
edwardribas <ribasedu2005@gmail.com>
2022-05-05 09:06:08 -0300

    Exercícios

---
+++ 5-maio/ex1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <!-- Metas -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- General -->
+    <title>Sistemas Web</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <!-- Form -->
+    <form action="" method="POST">
+        <input type="number" step="1" placeholder="Número" name="n1" autofocus>
+        <button>Calcular</button>
+    </form>
+
+    <?php
+        $num = $_POST["n1"];
+        $cubo = pow($num, 3);
+
+        if(!empty($num)){
+            for($i = $num; $i <= $cubo; $i++){
+                echo "$i \n";
+            }
+        }
+    
+    ?>
+</body>
+</html>
