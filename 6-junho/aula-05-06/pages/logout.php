24342310da8ad89076ca4aa9a9cf73143a42abb9
Eduardo Ribas <ribasedu2005@gmail.com>
2022-06-03 06:17:42 -0300

    Small change
    1 line changed

--- 6-junho/aula-05-06/pages/logout.php
+++ 6-junho/aula-05-06/pages/logout.php
@@ -2,7 +2,7 @@
     session_start();
 
     if (isset($_SESSION) || $_SESSION['booleanLogged'] === 1) {
-        unset($_SESSION);
+        session_unset($_SESSION);
         session_destroy();
         header("Location: ../.");
     }
